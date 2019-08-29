6c1f8d4642584ad5eee01d9ebdd7245a8accb313
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-29 02:40:09 +0000

    Automatically backed up by Learn

--- db/migrate/20190829023553_create_appointments.rb
+++ db/migrate/20190829023553_create_appointments.rb
@@ -1,7 +1,8 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
-
+      t.datetime :appointment_datetime
+      t.belongs_to :doctor
       t.timestamps
     end
   end
