dbaa88a2f8c7ed56387d2a2e3aaea7c5152bb808
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-29 03:42:15 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,6 @@ class Appointment < ApplicationRecord
   belongs_to :doctor
 
   def appointment_time
-    self.appointment_datetime.strftime(%B %d, %Y at %H: %M)
+    self.appointment_datetime.strftime("%B %d, %Y at %H: %M")
   end
 end
