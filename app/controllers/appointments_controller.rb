1c89c93d80f3ac73cfc8b8b67b3d27726a3ef5a3
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-29 02:48:52 +0000

    Automatically backed up by Learn

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,2 +1,30 @@
 class AppointmentsController < ApplicationController
+  def show
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    redirect_to appointment_path(@appointment)
+  end
+
+  def new
+    @appointment = Appointment.new
+  end
+
+  def edit
+    @appointment = Appointment.find(params[:id])
+  end
+
+  def update
+    @appointment = Appointment.find(params[:id])
+    @appointment.update(appointment_params)
+    redirect_to appointment_path(@appointment)
+  end
+
+  def destroy
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+    redirect_to appointment_path(@appointment)
+  end
 end
